9081e14320ec6e4422d46cdd3065eb4619dac121
githubacct0 <githubacct0@outlook.com>
2021-09-18 14:32:38 +0100

    cleared chart

--- client/src/views/Home/Home.vue
+++ client/src/views/Home/Home.vue
@@ -45,44 +45,31 @@ export default {
     dateAxis.renderer.grid.template.location = 0;
     dateAxis.renderer.labels.template.fill = am4core.color("#e59165");
 
-    var dateAxis2 = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis2.renderer.grid.template.location = 0;
-    dateAxis2.renderer.labels.template.fill = am4core.color("#dfcc64");
-
     var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.tooltip.disabled = true;
     valueAxis.renderer.labels.template.fill = am4core.color("#e59165");
 
     valueAxis.renderer.minWidth = 60;
 
-    var valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis2.tooltip.disabled = true;
-    valueAxis2.renderer.labels.template.fill = am4core.color("#dfcc64");
-    valueAxis2.renderer.minWidth = 60;
-    valueAxis2.syncWithAxis = valueAxis;
-
     var series = chart.series.push(new am4charts.LineSeries());
-    series.name = "2015";
+    // series.name = "2015";
     series.dataFields.dateX = "date1";
     series.dataFields.valueY = "value1";
     series.tooltipText = "{valueY.value}";
     series.fill = am4core.color("#e59165");
     series.stroke = am4core.color("#e59165");
-    //series.strokeWidth = 3;
+    series.strokeWidth = 3;
 
     var series2 = chart.series.push(new am4charts.LineSeries());
-    series2.name = "2017";
+    // series2.name = "2017";
     series2.dataFields.dateX = "date2";
     series2.dataFields.valueY = "value2";
-    series2.yAxis = valueAxis2;
-    series2.xAxis = dateAxis2;
     series2.tooltipText = "{valueY.value}";
     series2.fill = am4core.color("#dfcc64");
     series2.stroke = am4core.color("#dfcc64");
-    //series2.strokeWidth = 3;
+    series2.strokeWidth = 3;
 
     chart.cursor = new am4charts.XYCursor();
-    chart.cursor.xAxis = dateAxis2;
 
     var scrollbarX = new am4charts.XYChartScrollbar();
     scrollbarX.series.push(series);
@@ -92,8 +79,6 @@ export default {
     chart.legend.parent = chart.plotContainer;
     chart.legend.zIndex = 100;
 
-    valueAxis2.renderer.grid.template.strokeOpacity = 0.07;
-    dateAxis2.renderer.grid.template.strokeOpacity = 0.07;
     dateAxis.renderer.grid.template.strokeOpacity = 0.07;
     valueAxis.renderer.grid.template.strokeOpacity = 0.07;
 
