da5090b93b1fd6601ab4ed22fe8e87f3a499f86b
githubacct0 <githubacct0@outlook.com>
2021-09-19 12:14:31 +0500

    work in progress
    vertical scrollbar and horizontal added
    scale labels forced to month

--- client/src/components/SerieseChart.vue
+++ client/src/components/SerieseChart.vue
@@ -1,8 +1,53 @@
 <template>
-  <!-- bottom padding 48px because of fixed footer -->
-  <v-container fluid class="pl-6 pr-6 pt-6 pb-12">
-    <div class="hello" ref="chartdiv"></div>
-  </v-container>
+  <div>
+    <v-row align="center" justify="center">
+      <v-btn-toggle
+        v-model="zoomOption"
+        @change="zoomOptionSelected"
+        tile
+        color="primary"
+        group
+      >
+        <v-btn> Week </v-btn>
+        <v-btn> Month </v-btn>
+        <v-btn> Year </v-btn>
+        <v-btn> 2 Years </v-btn>
+      </v-btn-toggle>
+      <v-col cols="12" sm="6" md="4">
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Picker in menu"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title scrollable range>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+            <v-btn text color="primary" @click="$refs.menu.save(date)">
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <!-- bottom padding 48px because of fixed footer -->
+    <v-container fluid class="pl-6 pr-6 pt-6 pb-12">
+      <div class="hello" ref="chartdiv"></div>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -17,6 +62,9 @@ export default {
     chart: null,
     zoomStartDate: "2015-01-21",
     zoomEndDate: "2021-08-20",
+    zoomOption: "",
+    date: null,
+    menu: false,
   }),
   props: {
     highLightModule: {
@@ -31,19 +79,22 @@ export default {
     },
   },
   methods: {
+    zoomOptionSelected(v) {
+      console.log(v);
+    },
     drawChart() {
       // Themes begin
       let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
       let data = [];
       let data_1 = this.modelData[0].data.getModelMetrics.cagrReturns; //object_data_1.data.getModelMetrics.cagrReturns;
       let data_2 = this.modelData[1].data.getModelMetrics.cagrReturns; //object_data_2.data.getModelMetrics.cagrReturns;
-      
-      for (let i = data_1.length-1; i >= 0 ; i--) {
+
+      for (let i = data_1.length - 1; i >= 0; i--) {
         const date_breaker = new Date(data_1[i].effectiveDate); //.toUTCString();
         const value_breaker = data_1[i].spreadReturn;
         data.push({ date1: date_breaker, value1: value_breaker });
       }
-      for (let i = data_2.length-1; i >=0 ; i--) {
+      for (let i = data_2.length - 1; i >= 0; i--) {
         const date_breaker = new Date(data_2[i].effectiveDate); //.toUTCString();
         const value_breaker = data_2[i].spreadReturn;
         data.push({ date2: date_breaker, value2: value_breaker });
@@ -57,8 +108,13 @@ export default {
       chart.data = data;
 
       var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-      dateAxis.renderer.grid.template.location = 0;
+      // Set date label formatting
+      dateAxis.dateFormats.setKey("day", "MMMM dt");
+      dateAxis.periodChangeDateFormats.setKey("day", "MMMM dt");
+      //dateAxis.renderer.grid.template.location = 0;
       dateAxis.renderer.labels.template.fill = am4core.color("#e59165");
+      dateAxis.renderer.minGridDistance = 30;
+      
 
       var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
       valueAxis.tooltip.disabled = true;
@@ -88,8 +144,8 @@ export default {
       //series2.stroke = am4core.color("#dfcc64");
       series2.strokeWidth = 2;
 
-    //reduce opacity if highLightModule not matched
-    if (this.highLightModule != modelVersionName) {
+      //reduce opacity if highLightModule not matched
+      if (this.highLightModule != modelVersionName) {
         series.tooltip.background.opacity = 0.2;
         series.strokeOpacity = 0.3;
         series.fill = am4core.color(greyHex);
@@ -104,8 +160,15 @@ export default {
       chart.cursor = new am4charts.XYCursor();
 
       var scrollbarX = new am4charts.XYChartScrollbar();
-      scrollbarX.series.push(series);
+      if (this.highLightModule != modelVersionName)
+        scrollbarX.series.push(series);
+      else scrollbarX.series.push(series2);
       chart.scrollbarX = scrollbarX;
+      var scrollbarY = new am4charts.XYChartScrollbar();
+      if (this.highLightModule != modelVersionName)
+        scrollbarY.series.push(series);
+      else scrollbarY.series.push(series2);
+      chart.scrollbarY = scrollbarY;
 
       chart.legend = new am4charts.Legend();
       chart.legend.parent = chart.plotContainer;
@@ -127,6 +190,12 @@ export default {
       chart.zoomOutButton.valign = "bottom";
       chart.zoomOutButton.marginLeft = 10;
       chart.zoomOutButton.marginBottom = 10;
+      //chart options
+      //   chart.marginRight = 40;
+      //   chart.marginLeft = 40;
+      //   chart.autoMarginOffset = 20;
+      //   chart.mouseWheelZoomEnabled = true;
+      //   chart.dataDateFormat = "YYYY-MM-DD";
 
       this.chart = chart;
     },
